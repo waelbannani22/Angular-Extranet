6fe957fe3ae83f6cb7a69158ba16e0a4278dd7e6
waelbannani22 <79256781+waelbannani22@users.noreply.github.com>
2023-03-13 10:39:52 +0100

    ordonnance finie

--- src/app/component/conjoints/conjoints.component.ts
+++ src/app/component/conjoints/conjoints.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-conjoints',
   templateUrl: './conjoints.component.html',
